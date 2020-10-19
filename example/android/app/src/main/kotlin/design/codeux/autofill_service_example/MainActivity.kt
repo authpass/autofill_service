f1c3824f3f65fde9916d7e9a78c341f09a139737
Herbert Poul <herbert@codeux.design>
2020-10-19 11:00:30 +0200

    upgrade example to v2 plugin embedding

--- example/android/app/src/main/kotlin/design/codeux/autofill_service_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/design/codeux/autofill_service_example/MainActivity.kt
@@ -1,13 +1,6 @@
 package design.codeux.autofill_service_example
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
