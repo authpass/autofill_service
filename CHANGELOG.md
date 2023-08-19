bccf759282615302634938ddf2fd141be9a211bd
Herbert Poul <herbert@codeux.design>
2023-08-19 23:19:24 +0200

    Upgrade android gradle plugin, kotlin, etc.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1
+
+* Upgrade android gradle plugin, kotlin, etc.
+
 ## 0.1.0
 
 * null safety
--- android/src/main/java/design/codeux/autofill_service/SettingsActivity.kt
+++ android/src/main/java/design/codeux/autofill_service/SettingsActivity.kt
@@ -1,18 +1,20 @@
 package design.codeux.autofill_service
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_settings.*
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
